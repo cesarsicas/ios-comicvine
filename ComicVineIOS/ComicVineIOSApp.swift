672d8e885d35d8c2f52eb503e12b2daf46e2616d
cesarsicas <jctsiqueira@gmail.com>
2025-08-08 11:00:32 -0300

    Initial Commit

---
+++ ComicVineIOS/ComicVineIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicVineIOSApp.swift
+//  ComicVineIOS
+//
+//  Created by Julio Siqueira on 08/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ComicVineIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
